1a09f7c1c6f67c37e030ea813af0ed50df554270
Shiviiiiiiii <Lucasdele13@gmail.com>
2024-06-22 14:04:14 +0200

    Final project and deploys

--- appcigarette/src/components/RewardList.js
+++ appcigarette/src/components/RewardList.js
@@ -1,13 +1,16 @@
+// RewardList.js
+
 import React from 'react';
 
 const RewardList = ({ points, setPoints }) => {
   const rewards = [
-    { id: 1, text: 'Calins Illimité', cost: 5 },
+    { id: 1, text: 'Câlins Illimité', cost: 5 },
     { id: 2, text: 'Bisous Illimité', cost: 10 },
-    { id: 3, text: 'Massage', cost: 20 },
-    { id: 4, text: 'Bubble Tea', cost: 50 },
-    { id: 5, text: 'Restaurant au choix', cost: 150 },
-    { id: 6, text: 'Disney', cost: 300 },
+    { id: 3, text: 'Texte', cost: 15 },
+    { id: 4, text: 'Massage', cost: 20 },
+    { id: 5, text: 'Bubble Tea', cost: 50 },
+    { id: 6, text: 'Restaurant au choix', cost: 150 },
+    { id: 7, text: 'Disney', cost: 300 },
   ];
 
   const buyReward = (rewardId) => {
@@ -15,7 +18,7 @@ const RewardList = ({ points, setPoints }) => {
     if (points >= reward.cost) {
       console.log("Buy");
       setPoints(points - reward.cost);
-  
+
       // Envoi du message à Discord
       const message = `N'amour vient d'acheter la récompense : ${reward.text}`;
       fetch('https://discord.com/api/webhooks/1253843365305516104/yHjCt8nZCGKGeIf1aJUc-bYjykFwkAw8eIwvwqrQpmXXXgBEo0uLKkHshume_7zNApvD', {
@@ -35,11 +38,13 @@ const RewardList = ({ points, setPoints }) => {
   };
 
   return (
-    <div className="rewards">
+    <div className="reward-list-scrollable">
       {rewards.map(reward => (
-        <div key={reward.id} className={`reward-item`}>
-          <span>{reward.text}</span>
-           <button onClick={() => buyReward(reward.id)} disabled={points < reward.cost}>Acheter pour {reward.cost} points</button>
+        <div key={reward.id} className="reward-item">
+          <span className="reward-text">{reward.text}</span>
+          <button className="reward-button" onClick={() => buyReward(reward.id)} disabled={points < reward.cost}>
+            Acheter pour {reward.cost} points
+          </button>
         </div>
       ))}
     </div>
--- appcigarette/src/pages/Home.js
+++ appcigarette/src/pages/Home.js
@@ -1,23 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Realm from "realm-web";
 import DaysCounter from '../components/DaysCounter';
 import PointsCounter from '../components/PointsCounter';
 import CigaretteCounter from '../components/CigaretteCounter';
 import RewardList from '../components/RewardList';
 import TimeReset from '../components/TimeReset';
 
+const APP_ID = "data-cigarette-anvncfi"; // Remplacez par votre ID d'application Realm
+
 const Home = () => {
   const [days, setDays] = useState(0);
   const [points, setPoints] = useState(0);
   const [cigarettes, setCigarettes] = useState(0);
+  const [isLoading, setIsLoading] = useState(true); // Ajout de l'état isLoading
 
-  const handleCigaretteChange = (change) => {
-    setCigarettes(cigarettes + change);
+  useEffect(() => {
+    const fetchData = async () => {
+      const app = new Realm.App({ id: APP_ID });
+      const credentials = Realm.Credentials.anonymous();
+      try {
+        const user = await app.logIn(credentials);
+        const mongo = user.mongoClient("mongodb-atlas");
+        const collection = mongo.db("app-data").collection("data");
+        const data = await collection.find({});
+        if (data.length > 0) {
+          const latest = data[data.length - 1];
+          setDays(latest.days);
+          setPoints(latest.points);
+          setCigarettes(latest.cigarettes);
+        }
+        setIsLoading(false); // Données chargées, on met isLoading à false
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+        setIsLoading(false); // En cas d'erreur, on met aussi isLoading à false
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Chargement...</div>; // Affiche un message de chargement
+  }
+
+  const handleCigaretteChange = async (change) => {
+    const newCigarettes = cigarettes + change;
+    setCigarettes(newCigarettes);
+    await saveData(days, points, newCigarettes);
   };
 
-  const handleReset = () => {
-    setDays(days + 1);
-    setPoints(cigarettes === 0 ? points + 8 : cigarettes === 1 ? points + 4 : cigarettes === 2 ? points + 2 : cigarettes === 3 ? points + 1 : points - (cigarettes - 3));
+  const handleReset = async () => {
+    const newDays = days + 1;
+    const newPoints = cigarettes === 0 ? points + 8 : cigarettes === 1 ? points + 4 : cigarettes === 2 ? points + 2 : cigarettes === 3 ? points + 1 : points - (cigarettes - 3)
+    setDays(newDays);
+    setPoints(newPoints);
     setCigarettes(0);
+    
+    saveData(newDays, newPoints, 0);
+  };
+
+  const saveData = async (days, points, cigarettes) => {
+    const app = new Realm.App({ id: APP_ID });
+    const credentials = Realm.Credentials.anonymous();
+    try {
+      const user = await app.logIn(credentials);
+      const mongo = user.mongoClient("mongodb-atlas");
+      const collection = mongo.db("app-data").collection("data");
+  
+      const filter = { _id: "1" };
+  
+      const update = {
+        $set: {
+          days: days,
+          points: points,
+          cigarettes: cigarettes,
+        },
+      };
+  
+      const options = { upsert: true }; // Crée le document s'il n'existe pas
+  
+      const result = await collection.updateOne(filter, update, options);
+      console.log(`Document mis à jour avec succès: ${result.modifiedCount}`);
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour des données:", error);
+    }
   };
 
   return (
